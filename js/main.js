f90fbe9cd18332e6a0963e580152ec978d006b34
Vivian <vivian@Edgars-MacBook-Air.local>
2019-10-05 18:01:42 -0400

    final commit heh

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
+//javascript referenced from Stack Over Flow
+//functions here are solely for calculating the pricing based on amount
+var bun_amt = new Array();
+bun_amt["one"]=5;
+bun_amt["three"]=14;
+bun_amt["six"]=25;
+bun_amt["twelve"]=50;
 
- var bun_amt = new Array();
- bun_amt["one"]=20;
- bun_amt["three"]=25;
- bun_amt["six"]=35;
- bun_amt["twelve"]=75;
- 
- 
 function getBunPrice()
 {  
-    // clicked = true;
-    var amountPrice=0;
-    //Get a reference to the form id="bunform"
+    var amountPrice=5;
     var theForm = document.forms["bunform"];
-    //Get a reference to the cake the user Chooses name=selectedAmt":
     var selectedAmt = theForm.elements["selectedamount"];
 
     for(var i = 0; i < selectedAmt.length; i++)
@@ -26,23 +23,16 @@ function getBunPrice()
             break;
         }
     }
-    //We return the amountPrice
     return amountPrice;
 }
 
 
-
 function calculateTotal()
 {
-
-    
     var amtPrice = getBunPrice() 
     
     //display the result
     var divobj = document.getElementById('totalPrice');
     divobj.style.display='block';
     divobj.innerHTML = "<h3>$ "+amtPrice+"</h3>";
-
-
-
 }
